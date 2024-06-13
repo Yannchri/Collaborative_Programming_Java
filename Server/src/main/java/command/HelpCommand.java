db8104290a3ff88aeab438664b01526f46ee0da8
MarVer <86689557+MvUltimate@users.noreply.github.com>
2024-06-13 23:22:55 +0200

    Update the project

--- Server/src/main/java/command/HelpCommand.java
+++ Server/src/main/java/command/HelpCommand.java
@@ -9,18 +9,19 @@ public class HelpCommand implements Command {
     @Override
     public void execute(PrintWriter writer) {
         // Creating a StringBuilder to build the help message
-        StringBuilder helpMessage = new StringBuilder();
+        // Initialize the string for the help message
+        String helpMessage = "";
 
         // Appending available commands and their descriptions to the help message
-        helpMessage.append("Available commands:\n");
-        helpMessage.append("  deposit <amount> - Deposits <amount> into your account.\n");
-        helpMessage.append("  transfer <amount> <toClientId> - Transfers <amount> to another account.\n");
-        helpMessage.append("  balance - Displays your current balance.\n");
-        helpMessage.append("  transaction - Displays the list of your transactions.\n");
-        helpMessage.append("  help - Displays this help message.\n");
-        helpMessage.append("  exit - Disconnects from the server.\n");
+        helpMessage += "Available commands:\n";
+        helpMessage += "  deposit <amount> - Deposits <amount> into your account.\n";
+        helpMessage += "  transfer <amount> <toClientId> - Transfers <amount> to another account.\n";
+        helpMessage += "  balance - Displays your current balance.\n";
+        helpMessage += "  transaction - Displays the list of your transactions.\n";
+        helpMessage += "  help - Displays this help message.\n";
+        helpMessage += "  exit - Disconnects from the server.\n";
 
-        // Writing the help message to the PrintWriter
+        // Writing the help message using PrintWriter
         writer.println(helpMessage);
     }
 }
